1c81e557789b756f55b3b5c321eeb4e45803d2dc
Anqwert01 <Anqwert01@proton.me>
2025-08-24 19:34:38 +0530

    Add files via upload

---
+++ task1_student_marks.py
@@ -0,0 +1,14 @@
+student_marks = {
+    "Alice": 85,
+    "Bob": 90,
+    "Charlie": 78,
+    "David": 92
+}
+
+name = input("Enter the student's name: ")
+
+if name in student_marks:
+    print(f"{name}'s marks:")
+    print(student_marks[name])
+else:
+    print("Student not found.")
---
+++ task2_list_slicing.py
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+extracted = numbers[:5]
+
+reversed_list = extracted[::-1]
+
+print("Original list:", numbers)
+print("Extracted first five elements:", extracted)
+print("Reversed extracted elements:", reversed_list)
